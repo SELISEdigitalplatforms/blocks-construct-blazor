312a78d7d0f3a25c80a34cce8d77a6e76cd6cc85
Kazi Lakit <lakitur.rahman@selisegroup.com>
2026-04-03 04:24:43 +0600

    add new property

--- src/Client/Models/IAM/IamUser.cs
+++ src/Client/Models/IAM/IamUser.cs
@@ -11,6 +11,8 @@ public class IamUser
     public string? LastName { get; set; }
     public string Email { get; set; } = "";
     public string? PhoneNumber { get; set; }
+    public string? Department { get; set; }
+    public string? EmployeeId { get; set; }
     public List<string> Roles { get; set; } = [];
 
     [JsonPropertyName("status")]
